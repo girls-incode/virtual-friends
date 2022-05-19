ab17e6ead17ebf09fa58a9c36c7c1997cf5cdbe7
girlsincode <hola@girlsincode.com>
2022-05-19 20:23:23 +0200

    feat: user sign, out, edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :friends
   root 'home#index'
   get 'home/about'
